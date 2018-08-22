867d1e77915141938d3fa967bf960412f2c0bfc1
Jeffrey Berger <facelesspengwin@gmail.com>
2018-08-22 15:41:51 -0700

    Updated with better lazy loading

--- authentication-final/src/app/core/core.module.ts
+++ authentication-final/src/app/core/core.module.ts
@@ -3,6 +3,10 @@ import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { ShareModule } from '../shared/share.module';
 import { AppRoutingModule } from '../app-routing.module';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { RecipeService } from '../recipes/recipe.service';
+import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
 
 @NgModule({
   declarations: [
@@ -16,6 +20,7 @@ import { AppRoutingModule } from '../app-routing.module';
   exports: [
     HeaderComponent,
     AppRoutingModule
-  ]
+  ],
+  providers: [ShoppingListService, RecipeService, DataStorageService, AuthService]
 })
 export class CoreModule {}
