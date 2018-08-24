7aa7c40c4349014ee4bda042059fe46f559d66f8
Jeffrey Berger <facelesspengwin@gmail.com>
2018-08-23 17:14:35 -0700

    Added new HTTPClient requests and tutorial

--- authentication-final/src/app/core/core.module.ts
+++ authentication-final/src/app/core/core.module.ts
@@ -7,6 +7,8 @@ import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { RecipeService } from '../recipes/recipe.service';
 import { DataStorageService } from '../shared/data-storage.service';
 import { AuthService } from '../auth/auth.service';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from '../shared/auth.interceptor';
 
 @NgModule({
   declarations: [
@@ -21,6 +23,11 @@ import { AuthService } from '../auth/auth.service';
     HeaderComponent,
     AppRoutingModule
   ],
-  providers: [ShoppingListService, RecipeService, DataStorageService, AuthService]
+  providers: [
+    ShoppingListService,
+    RecipeService,
+    DataStorageService,
+    AuthService,
+    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}]
 })
 export class CoreModule {}
--- authentication-final/src/app/shared/data-storage.service.ts
+++ authentication-final/src/app/shared/data-storage.service.ts
@@ -1,31 +1,61 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
 import 'rxjs/Rx';
 
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
 import { AuthService } from '../auth/auth.service';
+import { HttpClient, HttpHeaders, HttpParams, HttpRequest } from '@angular/common/http';
 
 @Injectable()
 export class DataStorageService {
-  constructor(private http: Http,
+  constructor(private http: HttpClient,
               private recipeService: RecipeService,
               private authService: AuthService) {
   }
 
   storeRecipes() {
     const token = this.authService.getToken();
+    // const header = new HttpHeaders().set('Authorization', 'Bearer aibsdb3ububsdvu').append('header2', 'blah');
 
-    return this.http.put('https://angular-course-project-8f707.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes());
+    // return this.http.put('https://angular-course-project-8f707.firebaseio.com/recipes.json', this.recipeService.getRecipes(), {
+    //   observe: 'body',
+    //   params: new HttpParams().set('auth', token)
+    //   // headers: header
+    // });
+    const req = new HttpRequest('PUT', 'https://angular-course-project-8f707.firebaseio.com/recipes.json', this.recipeService.getRecipes(), {
+      reportProgress: true, params: new HttpParams().set('auth', token)
+    });
+    return this.http.request(req);
   }
 
   getRecipes() {
     const token = this.authService.getToken();
 
-    this.http.get('https://angular-course-project-8f707.firebaseio.com/recipes.json?auth=' + token)
+    // this.http.get('https://angular-course-project-8f707.firebaseio.com/recipes.json?auth=' + token, {
+    //   observe: 'response',   // body, response, etc...
+    //   responseType: 'blob'  // arrayBuffer, json, text, blob, etc...
+    // })
+    //   .map(
+    //     (recipes) => {
+    //       console.log(recipes);
+    //       for (const recipe of recipes) {
+    //         if (!recipe['ingredients']) {
+    //           recipe['ingredients'] = [];
+    //         }
+    //       }
+    //       return [];
+    //     }
+    //   )
+    //   .subscribe(
+    //     (recipes: Recipe[]) => {
+    //       this.recipeService.setRecipes(recipes);
+    //     }
+    //   );
+    this.http.get<Recipe[]>('https://angular-course-project-8f707.firebaseio.com/recipes.json', {
+      params: new HttpParams().set('auth', token)
+    })
       .map(
-        (response: Response) => {
-          const recipes: Recipe[] = response.json();
+        (recipes) => {
           for (const recipe of recipes) {
             if (!recipe['ingredients']) {
               recipe['ingredients'] = [];
