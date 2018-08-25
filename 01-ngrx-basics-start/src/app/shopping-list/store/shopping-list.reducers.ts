d25804e2c915dc124627fa8b32527d6e0aa67e40
Jeffrey Berger <facelesspengwin@gmail.com>
2018-08-24 17:12:37 -0700

    Adding in init of reduc tutorial

---
+++ 01-ngrx-basics-start/src/app/shopping-list/store/shopping-list.reducers.ts
@@ -0,0 +1,40 @@
+import * as ShoppingListActions from './shopping-list.actions';
+import { Ingredient } from '../../shared/ingredient.model';
+
+const initialState = {
+  ingredients: [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+  ]
+};
+
+export function shoppingListReducer(state = initialState, action: ShoppingListActions.ShoppingListActions) {
+  switch (action.type) {
+    case ShoppingListActions.ADD_INGREDIENT:
+      return {
+        ...state,
+        ingredients: [...state.ingredients, action.payload]
+      };
+    case ShoppingListActions.ADD_INGREDIENTS:
+      return {
+        ...state,
+        ingredients: [...state.ingredients, ...action.payload]
+      };
+    case ShoppingListActions.UPDATE_INGREDIENT:
+      const ingredient = state.ingredients[action.payload.index];
+      const updatedIngredient = {
+        ...ingredient
+      }
+      return {
+        ...state,
+        ingredients: [...state.ingredients, action.payload]
+      };
+    case ShoppingListActions.DELETE_INGREDIENT:
+      return {
+        ...state,
+        ingredients: [...state.ingredients, ...action.payload]
+      };
+    default:
+      return state;
+  }
+}
